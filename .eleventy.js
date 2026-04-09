ff56e59c3d2e2488227a5a1b2a3dffcba149599a
maskoala <lenezgrand@gmail.com>
2026-04-09 13:37:06 +0800

    Add custom domain CNAME for choonie.me

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/CNAME");
 
   return {
     dir: {
