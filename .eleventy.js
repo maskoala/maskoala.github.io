c47a09d978f59743be548b809a111737dc663b5e
maskoala <lenezgrand@gmail.com>
2026-04-09 13:33:26 +0800

    Remove pathPrefix — repo renamed to maskoala.github.io

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images");
 
   return {
-    pathPrefix: "/choonie-portfolio/",
     dir: {
       input: "src",
       output: "_site",
